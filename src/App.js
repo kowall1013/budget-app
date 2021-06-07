155457ef692844055c683c41b48212be11ca463b
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-06-07 13:46:18 +0200

    add basic Navigation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Navigation } from "components";
+import { ThemeProvider } from "styled-components";
+import React from "react";
+import theme from "utils/theme";
+import GlobalStyle from "index.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
-  return <h1>budget</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
+      <Router>
+        <Navigation
+          items={[
+            { content: "Hompage", to: "/" },
+            { content: "Budget", to: "/budget" },
+          ]}
+        />
+        <Switch>
+          <Route exact path="/">
+            Homepage
+          </Route>
+          <Route path="/budget">BudgetPage</Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
