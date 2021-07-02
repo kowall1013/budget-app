9b3e99bff78920caa399012ff176238b83bc7ce4
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-07-02 09:58:16 +0200

    Replace redux with Context API using useState

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./i18n/i18n";
-import { Provider } from "react-redux";
 
 import RootApp from "./App";
-import configureStore from "./data/store";
-
-const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <RootApp />
-  </Provider>,
+  <RootApp />,
+
   document.getElementById("root")
 );
